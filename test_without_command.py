731a499a63345ceaefb9bc4db64b95afed2b6d4b
nico <nicolas.portal@amundi.com>
2019-10-22 16:18:23 +0200

    exclude wrong equipements

--- test_without_command.py
+++ test_without_command.py
@@ -1,14 +1,13 @@
 import paramiko
-import logging
 import time
 import socket
 import sys
 import threading
+import getpass
 from math import floor
 import queue
 from timeit import default_timer
 from multiprocessing.dummy import Process
-import getpass
 
 #Cette "class" permet de créer un objet "my_shell "qui va permettre de réaliser la connexion et d'envoyer les commandes de manière simplifiee. On retourne True quand il y a une erreur et False quand c'est ok
 class my_shell:
@@ -21,6 +20,7 @@ class my_shell:
         self.channel = paramiko.Channel(1)
         self.package = ()
         self.paramiko_exception = paramiko_exception
+
     #Le destructeur appele lorsque l'objet meurt, on ferme la connexion lorsqu'il disparait
     def __del__(self):
         self.ssh.close()
@@ -28,17 +28,24 @@ class my_shell:
     def init(self):
         try:
             self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            #print("Connecting to: {}...".format(self.device[1]))
-            self.ssh.connect(**self.device[0], allow_agent = False, look_for_keys = False, timeout=10, auth_timeout=15, banner_timeout=15)
+            self.ssh.connect(**self.device[0], allow_agent = False, look_for_keys = False, timeout=15, auth_timeout=15, banner_timeout=15)
             self.channel = self.ssh.invoke_shell()
         except self.paramiko_exception as erreur:
-            self.ssh.close()
+            if erreur == paramiko.ssh_exception.NoValidConnectionsError:
+                self.package = str(erreur) + " ------> " + str(erreur.errors) + ": " + self.device[1] + "\n", "wrong_file"
+            elif erreur == paramiko.ssh_exception.BadHostKeyException:
+                self.package = str(erreur) + " ------> " + "hostname of the ssh server: " + str(erreur.hostname) + " the host key presented by the server: " + str(erreur.got_key) + " the host key expected: " + str(erreur.expected_key) + ": " + self.device[1] + "\n", "wrong_file"
+            elif erreur == paramiko.ssh_exception.ChannelException:
+                self.package = str(erreur) + " ------> " + " error code returned by the server: " + str(erreur.code) + ": " + self.device[1] + "\n", "wrong_file"
             self.package = str(erreur) + ": " + self.device[1] + "\n", "wrong_file"
             return True
         return False
     #"Getter" pour avoir acces au package en dehors de la classe
     def get_package(self):
         return self.package
+    #"Getter" pour avoir acces au package en dehors de la classe
+    def get_device(self):
+        return self.device
     #"Setter" pour mettre a jour la valeur du package en dehors de la classe
     def set_package(self, message, file):
         self.package = message, file
@@ -48,7 +55,6 @@ class my_shell:
         t = default_timer()
         while True:
             if default_timer() - t >= 15:
-                self.ssh.close()
                 self.package = "The device took too much time to process password: {}\n".format(self.device[1]), "wrong_file"
                 return True
             self.get_info()
@@ -67,21 +73,22 @@ class my_shell:
             if self.s.endswith("Password: ") or self.s.endswith("password: ") or self.s.endswith("password:") or self.s.endswith("Password:") or self.s.endswith("#") or self.s.endswith("# "):
                 return False
             elif self.s.endswith("> ") or self.s.endswith(">") or self.s.endswith("$") or self.s.endswith("$ "):
-                self.ssh.close()
                 self.package = "Can't enter enable mode, probably different OS: {} \n".format(self.device[1]), "wrong_file"
                 return True
             elif default_timer() - t >= 15:
-                self.ssh.close()
                 self.package = "Took too much time to process \"en\" command: {} \n".format(self.device[1]), "wrong_file"
                 return True
     #Methode pour envoyer une commande au shell.Si le temps d'execution est superieur a "seconde" alors c'est trop long et on renvoit une erreur
     def send_command(self, cmd, seconds):
-        a = self.s
-        self.channel.send(cmd + "\n")
         t = default_timer()
+        a = self.s
+        try:
+            self.channel.send(cmd + "\n")
+        except self.paramiko_exception as error:
+            self.package = str(error) + ": " + self.device[1] + "\n", "wrong_file"
+            return True
         while True:
             if default_timer() - t >= seconds:
-                self.ssh.close()
                 self.package = "The device took too much time to process command: {}, {}\n".format(cmd, self.device[1]), "wrong_file"
                 return True
             self.get_info()
@@ -91,27 +98,22 @@ class my_shell:
     def enable(self):
         try:
             if self.s[-2] != "#" and self.s[-1] != "#":
-                #print("Entering enable mode...")
-                if self.send_command("en", 15):
+                if self.send_command("en", 25):
                     return True
                 #On verifie que la commande "en" a ete traitee, on gere les differents cas.
                 if self.checking():
                     return True
                 #Une fois que la commande "en" a ete traitee, on envoit le mot de passe enable.
-                if self.send_command(str(self.secret), 15):
+                if self.send_command(str(self.secret), 25):
                     return True
                 #Si apres avoir entre la mot de passe notre prompt ne se termine pas par "#" alors il y a eu un probleme.
                 if self.s[-2] != "#" and self.s[-1] != "#":
-                    self.ssh.close()
                     self.package = "Unable to connect to: {}, enable password not valid\n".format(self.device[1]), "wrong_file"
                     return True
         #Si il y a une erreur on la mets dans le "package" et on ferme la connexion.
         except self.paramiko_exception as erreur:
-            self.ssh.close()
             self.package = str(erreur) + ": " + self.device[1] + "\n", "wrong_file"
             return True
-        #Si on est la c'est qu'on a reussi a se connecter mais on a pas encore lance de commandes donc pour l'instant on mets dans le package qu'il y a eu une erreur sur les commandes(ce sera modifie plus tard dans le code si on arrive a lancer les commandes).
-        self.package = "{} {}\n".format(self.device[0]["hostname"], self.device[1]), "correct_file"
         return False
 
 #Fonction pour separer la liste d'equipements en "number_of_slices" sous liste (une pour chaque thread).
@@ -126,22 +128,43 @@ def slice_my_list(devices, number_of_slices):
     return my_list_of_list
 
 #Fonction pour ecrire dans le fichier (appelee par le thread ecrivain).
-def printfile(message, filename):
-    with open(filename, "a") as file:
-        file.write(message)
+def printfile(console):
+    my_package = console.get_package()
+    with open(my_package[1], "a") as file:
+        file.write(my_package[0])
+    with open("logging_info", "a") as logfile:
+        logfile.write("*" * 500 + "\n")
+        logfile.write(console.s)
 
-#Fonction pour mettre au travail le thread ecrivain, il recupere son travail sur la queue.
+#Fonction pour mettre au travail le thread ecrivain, il recupere son travail sur la queue. On incrémente la barre de chargement d'une unité lorsque l'ecrivain a écrit un équipement dans un fichier.
 def run_printer(queue, number):
-    sys.stdout.write("[%s]" % (" " * number))
-    sys.stdout.flush()
+    print("[%s]" % (" " * number), end="", flush=True)
     sys.stdout.write("\b" * (number + 1))
     while True:
-        my_tuple = queue.get()
-        printfile(*my_tuple)
+        console = queue.get()
+        printfile(console)
         queue.task_done()
-        print("#")
-        #sys.stdout.write("#")
-        #sys.stdout.flush()
+        del console
+        print("#", end="", flush=True)
+
+
+def put_min_queue(queue_device, device):
+    min = 1000
+    min_queue = queue.Queue()
+    for t in queue_device:
+        n = queue_device[t].qsize()
+        if n < min:
+            min = n
+            min_queue = queue_device[t]
+    min_queue.put((device[0], device[1]))
+
+def create_device(console):
+    device = {}
+    my_device = console.get_device()
+    device["hostname"] = my_device[0]["hostname"]
+    device["username"] = "network"
+    device["password"] = my_device[0]["password"]
+    return device, my_device[1]
 
 # Fonction qui realise la connexion.
 def process(device, paramiko_exception, secret):
@@ -149,24 +172,26 @@ def process(device, paramiko_exception, secret):
     console = my_shell(device, paramiko_exception, secret)
     #On se connecte avec notre objet et si il y a une erreur, on met le package sur la queue.
     if console.init():
-        return console.get_package()
+        return console
     #On attends que le mot de passe soit traite
     if console.wait_password_processing():
-        return console.get_package()
+        return console
     #On recoit les infos du shell.
     console.get_info()
     #On se connecte en mode enable.
     if console.enable():
-        return console.get_package()
-    #print("Connected to {}".format(device[1]))
-    return console.get_package()
+        return console
+    #Si on arrive ici c'est que tout a fonctionner et on peut donc mettre a jour le package pour le signifier.
+    console.set_package("Connected to {}\n".format(console.device[1]), "correct_file")
+    #Si on arrive ici c'est qu'on est connecté en mode enable on peut donc l'écrire dans le bon fichier.
+    return console
 
 #Fonction pour produire et mettre sur la chaine (la queue) qui est appelee avec start()
-def run_worker(queue_in, queue_out, paramiko_exception, secret):
+def run_worker_stage_1(queue_in, queue_out, paramiko_exception, secret):
     while True:
         job = queue_in.get()
-        my_tuple = process(job, paramiko_exception, secret)
-        queue_out.put(my_tuple)
+        shell = process(job, paramiko_exception, secret)
+        queue_out.put(shell)
         queue_in.task_done()
 
 #Mot de passe.
@@ -174,8 +199,15 @@ password = getpass.getpass(prompt="Enter password:")
 secret = getpass.getpass(prompt="Enter enable password:")
 
 #Exceptions à "catcher" en cas d'erreur.
-paramiko_exception = (paramiko.ssh_exception.NoValidConnectionsError,paramiko.ssh_exception.BadAuthenticationType,paramiko.ssh_exception.AuthenticationException,paramiko.ssh_exception.BadHostKeyException,paramiko.ssh_exception.ChannelException,paramiko.ssh_exception.PartialAuthentication,paramiko.ssh_exception.PasswordRequiredException,paramiko.ssh_exception.ProxyCommandFailure,paramiko.ssh_exception.SSHException,socket.timeout,
+paramiko_exception = (paramiko.ssh_exception.NoValidConnectionsError,paramiko.ssh_exception.BadAuthenticationType,paramiko.ssh_exception.AuthenticationException,paramiko.ssh_exception.BadHostKeyException,paramiko.ssh_exception.ChannelException,paramiko.ssh_exception.PartialAuthentication,paramiko.ssh_exception.PasswordRequiredException,paramiko.ssh_exception.ProxyCommandFailure,
+paramiko.ssh_exception.SSHException,
+paramiko.SSHException,
+socket.timeout,
+EOFError,
+ConnectionError,
+ConnectionResetError,
 socket.error,
+OSError,
 ValueError,
 IndexError)
 
@@ -184,31 +216,35 @@ devices = []
 
 #Un dictionnaire = un équipement.
 dico = {}
+#Tuple contennant les substrings qui indiquent que l'impossibilite de se connecter a l'equipement est normale
+list_of_substring_name = ["fw", "rb", "ucs", "vip", "evergreen", "wifi", "etr", "cyc", "bos0", "ltm", "proxy", "prx", "f5", "hsrp", "ise", "gw-", "loop", "-vl", "hsm", "dct", "rto", "ap", "gtm", "sydney"]
 
+list_of_sub_ip = ["10.250.", "10.198.237."]
 #Liste des ip qui correspond a la premiere partie de chaque ligne du fichier
 list_of_ip = []
+#Liste des noms qui correspond a la deuxième partie de chaque ligne du fichier
 list_of_name = []
-
-#Initialisation du logging qui sera écrit dans le fichier "logging_info".
-logging.basicConfig(filename = "logging_info", filemode = "w", format = "[%(levelname)s]: %(asctime)s %(message)s", level = logging.DEBUG)
+#Function to rule out devices with wrong names
+def dont_care(list_of_substring, equipment_name):
+    for substring in list_of_substring:
+        if substring in equipment_name:
+            return True
+    return False
 
 #Ouverture du fichier des équipements et stockage de chaque équipement dans une liste.
-with open("/home/network/host.txt", "r") as input_file:
+with open("host.txt", "r") as input_file:
     list_of_switches = input_file.readlines()
 
 #On enleve la premiere ligne du fichier qui ne nous interesse pas
 del(list_of_switches[0])
 
-#On recupere la premiere partie de chaque ligne du fichier (les adresses ip)
+#On recupere la premiere et la seconde partie de chaque ligne du fichier (les adresses ip et nom de l'equipement). Si l'equipement contient une substring qui permet de l'identifier comme étant ininteressant, on n'en tient pas compte.
 for i in list_of_switches:
     temp_list = i.split()
-    if "fw" in temp_list[1].lower() or "rb" in temp_list[1].lower():
+    if dont_care(list_of_substring_name, temp_list[1].lower()) or dont_care(list_of_sub_ip, temp_list[0]):
         continue
     list_of_ip.append(temp_list[0])
     list_of_name.append(temp_list[1])
-
-#Variable initialisé à 0 et qui sera incrémentée à chaque commande traitée de manière à passser à l'équipement suivant lorsque nb = nombre de commandes.
-nb = 0
 
 #Stockage de l'adresse ip de chaque équipement dans un dictionnaire différent. Tous les dictionnaire sont stockés dans la liste de dico "devices".
 for switch in list_of_ip:
@@ -218,9 +254,7 @@ for switch in list_of_ip:
     devices.append(dico.copy())
 
 #Le dictionnaire de queue qui va contenir n queue, une pour chaque thread
-dictionary_of_queues = {}
-
-list_of_threads = []
+dictionary_of_queues_first_layer = {}
 
 nb_of_threads = 0
 
@@ -236,13 +270,11 @@ else:
     my_list_of_lists = slice_my_list(devices, nb_of_threads)
     list_of_list_of_name = slice_my_list(list_of_name, nb_of_threads)
 
-tuple_of_device_and_name = ()
-
-#On remplie chaque queue du dictionnaire
+#On remplie chaque queue du dictionnaire avec un tuple qui va contenir deux elements: l'equipement sous forme de dictionnaire et le nom de l'equipement. C'est la partie la plus complexe du code. On itère sur 2 liste simultanément avec zip.
 for i, slice_of_devices in enumerate(my_list_of_lists):
-    dictionary_of_queues["queue_of_devices_" + str(i)] = queue.Queue()
+    dictionary_of_queues_first_layer["queue_of_devices_" + str(i)] = queue.Queue()
     for device, name in zip(slice_of_devices, list_of_list_of_name[i]):
-        dictionary_of_queues["queue_of_devices_" + str(i)].put((device, name))
+        dictionary_of_queues_first_layer["queue_of_devices_" + str(i)].put((device, name))
 
 #On initialise la queue "out" qui va etre traitee par le thread ecrivain (il y a donc 9 queue en tout).
 print_queue = queue.Queue()
@@ -250,23 +282,25 @@ print_queue = queue.Queue()
 #On tronque les deux fichiers pour qu'ils soient vide avant d'etre traites
 open("correct_file", "w").close()
 open("wrong_file", "w").close()
+open("logging_info", "w").close()
 
+logging_file = open("logging_info", "a")
 #On lance le thread ecrivain
 p = Process(target = run_printer, args = (print_queue, number_of_slices))
 p.daemon = True
 p.start()
 
-
 #On lance les threads producteur qui font les connexions, chaque thread travaille sur une partie de la liste des equipements.
 for i in range(nb_of_threads):
-    t = Process(target = run_worker, args = (dictionary_of_queues["queue_of_devices_" + str(i)], print_queue, paramiko_exception, secret))
+    t = Process(target = run_worker_stage_1, args = (dictionary_of_queues_first_layer["queue_of_devices_" + str(i)], print_queue, paramiko_exception, secret))
     t.daemon = True
     t.start()
 
 #On attends que tous les equipements soient traites (que les queues soient vides).
+for i in dictionary_of_queues_first_layer.keys():
+    dictionary_of_queues_first_layer[i].join()
+
 print_queue.join()
 
-for i in dictionary_of_queues.keys():
-    dictionary_of_queues[i].join()
-
+#On vide le buffer stdout pour que le prompt n'écrase pas la barre de chargement.
 print("")
